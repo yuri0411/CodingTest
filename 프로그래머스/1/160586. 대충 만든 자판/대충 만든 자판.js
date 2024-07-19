6ed523a1b8820ae610590579b94e4e83ceb233f0
강유리 <yu_ri0411@naver.com>
2024-07-19 12:08:43 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.73 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,23 @@
+function solution(keymap, targets) {
+    const keyMap = {}
+
+    for(let key of keymap) {
+        for(let i = 0; i < key.length; i++) {
+            if(keyMap[key[i]]) {
+                keyMap[key[i]] =  keyMap[key[i]] > i+1 ? i+1 :  keyMap[key[i]]
+            } else {
+                keyMap[key[i]] = i + 1
+            }
+        }
+    }
+    
+    return targets.map((target) => {
+        let sum = 0
+        for(let char of target) {
+            if(!keyMap[char]) return -1
+            sum += keyMap[char]
+        }
+        
+        return sum
+    })
+}
