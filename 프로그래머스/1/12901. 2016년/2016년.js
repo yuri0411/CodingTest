4b752d06102dc4ce363e990c028bf613f505fbcc
강유리 <yu_ri0411@naver.com>
2024-07-22 20:51:53 +0900

    변수 할당

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,12 +1,12 @@
 function solution(a, b) {
     const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     const daysOfWeek = [ "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-
+    const startDay = daysOfWeek.indexOf('FRI')
+    
     let days = b - 1; 
     for (let i = 0; i < a - 1; i++) {
         days += daysInMonth[i];
     }
 
-
-    return daysOfWeek[(days % 7 + 5) %7];
+    return daysOfWeek[(days % 7 + startDay) % 7];
 }
