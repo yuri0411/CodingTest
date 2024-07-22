b20334ee4e9b9b7874f0994594a0b116c57e2ba0
강유리 <yu_ri0411@naver.com>
2024-07-22 20:48:58 +0900

    [level 1] Title: 2016년, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,13 +1,12 @@
 function solution(a, b) {
-    const day = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
-    const doomsDays = [4, 29, 7, 4, 9, 6, 11, 8, 5, 10, 7, 12]
+    const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const daysOfWeek = [ "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-     const doomsday = ((Math.floor(16 / 12) + (16 % 12) + (16 % 12) % 4 + 2) / 7)
-    
-    if(doomsDays[a - 1] <= b) {
-        return day[doomsday + Math.abs(doomsDays[a - 1] - b) % 7]
-    } else {
-        
-        return day[(doomsday + 7 - (doomsDays[a - 1] - b)) % 7]
+    let days = b - 1; 
+    for (let i = 0; i < a - 1; i++) {
+        days += daysInMonth[i];
     }
+
+
+    return daysOfWeek[(days % 7 + 5) %7];
 }
