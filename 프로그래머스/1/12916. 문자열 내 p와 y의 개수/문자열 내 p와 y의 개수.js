96380fbaa0c6d682b48082bffcc27045358d95d2
강유리 <yu_ri0411@naver.com>
2024-07-25 15:38:08 +0900

    Update 문자열 내 p와 y의 개수.js

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -3,3 +3,7 @@ function solution(s) {
    
     return chars.filter((char) => char === "p").length === chars.filter((char) => char === "y").length
 }
+
+
+// match 메서드와 정규표현식을 이용한 풀이
+// return s.match(/p/ig).length == s.match(/y/ig).length
