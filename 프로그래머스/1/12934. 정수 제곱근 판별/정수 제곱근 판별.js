e1ec06bd5677a1a303b3f603068805108f652fa8
강유리 <yu_ri0411@naver.com>
2024-08-04 23:27:02 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,6 +1,5 @@
 function solution(n) {
     const sqrt = Math.sqrt(n)
-    const pow = Math.trunc(sqrt)
     
-    return sqrt === pow ? Math.pow(sqrt + 1, 2) : -1
+    return Number.isInteger(sqrt)  ? Math.pow(sqrt + 1, 2) : -1
 }
