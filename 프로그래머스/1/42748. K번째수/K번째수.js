422d892f7bbac48646165be54af6216aa8e6dce6
강유리 <yu_ri0411@naver.com>
2024-08-08 23:35:30 +0900

    [level 1] Title: K번째수, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,6 +1,10 @@
 function solution(array, commands) {
-    return commands.map((command) => {
-        const sliceArr = array.filter((_, index) =>  index >= command[0] - 1 && index <= command[1] - 1).sort((a, b) => a - b);
-        return sliceArr[command[2] - 1]
+     return commands.map((command) => {
+        let [start, end, index] = command
+        start--, end--, index--
+         
+        const sliceArr = array.filter((_, i) =>  i >= start && i <= end).sort((a, b) => a - b);
+        
+        return sliceArr[index]
     })
 }
