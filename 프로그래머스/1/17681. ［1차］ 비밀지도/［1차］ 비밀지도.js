62d81dd57c461018c191aefd710c6d50945e3462
강유리 <yu_ri0411@naver.com>
2024-08-08 23:08:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -3,6 +3,6 @@ function solution(n, arr1, arr2) {
     return arr1.map((num, index) => {
         const bitOr =  (num | arr2[index]).toString(2).padStart(n, '0');
 
-        return bitOr.replace(/1/g, "#").replace(/0/g, " ")
+        return bitOr.replace(/[1|0]/g, (s) => +s ? "#" : " ")
     } )
 }
