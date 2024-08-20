b8e85ee02017954e0860664131c8a655bed56acb
강유리 <yu_ri0411@naver.com>
2024-08-21 01:21:57 +0900

    [level 1] Title: 최소직사각형, Time: 23.85 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,20 +1,5 @@
-
 function solution(sizes) {
-    const maxWidth= Math.max(...sizes.map((size) => size[0]))
-    const maxHeight = Math.max(...sizes.map((size) => size[1]));
+    const minMaxSizes = sizes.map((size) => [Math.min(...size), Math.max(...size)])
 
-    for (let i = 0; i < sizes.length; i++) {
-        let current = sizes[i]
-        if(maxHeight > maxWidth) {
-            if(current[0] > current[1] && current[0] < maxHeight) {
-                sizes[i] = [current[1], current[0]]
-            }
-        } else {
-            if(current[0] < current[1] && current[0] < maxWidth) {
-                sizes[i] = [current[1], current[0]]
-            }
-        }
-    }
-
-    return Math.max(...sizes.map((size) => size[0])) * Math.max(...sizes.map((size) => size[1]));
-}
+    return Math.max(...minMaxSizes.map((size) => size[0])) * Math.max(...minMaxSizes.map((size) => size[1]))
+ }
