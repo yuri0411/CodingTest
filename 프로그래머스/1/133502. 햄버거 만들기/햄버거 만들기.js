d395b1e52f1678e85d2599ac2342337c3940d0c5
강유리 <yu_ri0411@naver.com>
2024-08-28 20:13:07 +0900

    [level 1] Title: 햄버거 만들기, Time: 219.08 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -4,7 +4,7 @@ function solution(ingredient) {
 
       for (let i = 0; i < ingredient.length; i++) {
           stack.push(ingredient[i]);
-          if(stack.length >= 4 && stack.slice(-4).join() === [1,2,3,1].join()){
+          if(stack.length >= 4 && stack.slice(-4).join('') === '1231'){
               stack.splice(-4)
               answer++
           }
